58223151cadc828a36541530be13fc231ab67bf9
augsun <codersun@126.com>
2019-07-01 19:32:33 +0800

    push by autoRelease_JXEfficient.py

--- JXEfficient/Classes/JXEfficient/UIView/JXPopupView/JXPopupView.h
+++ JXEfficient/Classes/JXEfficient/UIView/JXPopupView/JXPopupView.h
@@ -39,7 +39,7 @@ typedef NS_ENUM(NSUInteger, JXPopupViewCloseTo) {
 @property (nonatomic, assign) CGFloat popupBgViewContentEdgeB; ///< popupBgView 子控件结束位置底边距, 默认 8.0
 
 // titleView
-@property (nonatomic, readonly) UIView *titleView; ///< popupBgView -> titleView <标题视图>, (若自定义, 加在该 View 上).
+@property (nonatomic, readonly) UIView *titleView; ///< popupBgView -> titleBgView -> titleView <标题视图>, (若自定义, 加在该 View 上).
 @property (nonatomic, assign) CGFloat titleViewToAboveWidget; ///< 距上个控件的间距, 如果为第一个控件, 则取 popupBgViewContentEdgeT. 注: titleView 目前为第一个控件, 该属性暂无用.
 @property (nonatomic, assign) UIEdgeInsets titleViewEdgeInsets; ///< 默认 {0.0, 8.0, 0.0, 8.0}
 @property (nonatomic, assign) CGFloat titleViewContentH; ///< 不包含 titleViewEdgeInsets 的高度, 为 0.0 时隐藏 titleView.
@@ -51,7 +51,7 @@ typedef NS_ENUM(NSUInteger, JXPopupViewCloseTo) {
 @property (nonatomic, assign) CGFloat contentViewContentH; ///< 不包含 contentViewEdgeInsets 的高度, 内容区域, 可以设置很大, 将进行滚动. 为 0.0 时隐藏 contentView.
 
 // buttonsView
-@property (nonatomic, readonly) UIView *buttonsView; ///< popupBgView -> buttonsView <按钮视图>, (若自定义, 加在该 View 上).
+@property (nonatomic, readonly) UIView *buttonsView; ///< popupBgView -> buttonsBgView -> buttonsView <按钮视图>, (若自定义, 加在该 View 上).
 @property (nonatomic, assign) CGFloat buttonsViewToAboveWidget; ///< 距上个控件的间距, 如果为第一个控件, 则取 popupBgViewContentEdgeT.
 @property (nonatomic, assign) UIEdgeInsets buttonsViewEdgeInsets; ///< 默认 {0.0, 0.0, 0.0, 0.0}
 @property (nonatomic, assign) CGFloat buttonsViewContentH; ///< 不包含 buttonsViewEdgeInsets 的高度, 为 0.0 时隐藏 buttonsView.
